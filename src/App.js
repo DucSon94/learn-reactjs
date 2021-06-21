9efdbaa28ad56878f8137ed2e39743742655a99d
sonnd <ducson395@gmail.com>
2021-06-22 00:26:15 +0700

    create component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Son';
+  const age =18;
+  const isMale =true;
+  const student ={
+    name: 'Easy Frontend'
+  };
+  const colorList =['red','green','blue']
   return (
     <div className="App">
       <header className="App-header">
@@ -9,10 +16,35 @@ function App() {
         <p>
          Sơn Nguyễn
         </p>
-       
+       <p>Xin chao {name} - {age} -{isMale ? 'Male' : 'Fermale'} </p>
+       {isMale ? <p>Male</p> : <p>Fermale</p>}
+       {isMale && <p>Male</p>}
+       {!isMale && <p>Female</p>}
+       {isMale &&(
+         <div>
+         <p>Male 1</p>
+         <p>Male 2</p>
+         <p>Male 3</p>
+         </div>
+       )}
+       {isMale && (
+         <>
+         <p>Male 1</p>
+         <p>Male 2</p>
+         <p>Male 3</p>
+         </>
+       )}
+       <ul>
+       {colorList.map((color ,index) =>(
+         <li style={{color}}>{index} - {color} </li>
+
+       ))}
+       </ul>
       </header>
     </div>
+    
   );
+ 
 }
 
 export default App;
